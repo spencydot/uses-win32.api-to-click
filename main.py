8bd03c7a694699116b89b6f7616d3bfa5653279c
spencydot <spencerjtrott@gmail.com>
2022-03-29 15:39:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import win32api, win32con
+import time
+
+time.sleep(2)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+click(100,100)
